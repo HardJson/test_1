f6f8f708432739c5b2ff47e90f42ab0e2b01eedf
HardJson <tony1353855@163.com>
2019-11-11 00:24:55 +0800

    springcloud 实现原理

--- src/main/java/com/alone/DbStart.java
+++ src/main/java/com/alone/DbStart.java
@@ -2,8 +2,13 @@ package com.alone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 import tk.mybatis.spring.annotation.MapperScan;
 
+/**
+ * 一个springboot项目的demo
+ */
 
 @SpringBootApplication
 @MapperScan("com.alone.mapper")
@@ -13,3 +18,18 @@ public class DbStart {
         SpringApplication.run(DbStart.class,args);
     }
 }
+
+/**
+ * 一个springcloud项目原理的demo,用于监听上面的demo但是要是两个项目启动
+ * RestTemplate 默认使用HttpUrlConnection
+ */
+/*public class DbStart {
+    public static void main(String[] args) {
+        SpringApplication.run(DbStart.class, args);
+    }
+
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+}*/
